dc96140985948fa32dfe04e4b4fc5a5040e3fab3
Andrii Riabchenko <ryabchenko.andrew@gmail.com>
2023-11-17 12:24:40 +0200

    maximize window

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  mainWindow.maximize()
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
